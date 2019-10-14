1bf1baffd5e380b52e9918e501ed8eec76796f9a
Deyer <meyer.david221@gmail.com>
2019-10-14 11:00:00 -0600

    Kinda done, need to fix the automatic thresholding section.

--- SkinDetection.m
+++ SkinDetection.m
@@ -8,62 +8,72 @@ picPathGood = './face_good.bmp';
 picPathDark = './face_dark.bmp';
 picGood = imread(picPathGood);
 picDark = imread(picPathDark);
-figure, montage({picGood,picPathDark})
+%figure, montage({picGood,picPathDark})
+skinGood = skinDetect(picGood);
+figure,imshow(skinGood)
+skinGood = uint8(skinGood);
 
-goodSkin = skinDetect(picGood);
-darkSkin = skinDetect(picDark);
+finalGood = picGood .* skinGood;
+figure,imshow(finalGood)
 
-%RGB values
-R=picDark(:,:,1);
-G=picDark(:,:,2);
-B=picDark(:,:,3);
-%"Transformation simplicity and explicit separation of luminance and
-%chrominance componenets makes this colorspace attractive for skin color
-%modelling"
-%So, Convert to YCbCr color space.
-Y = 0.299*R + 0.587*G + 0.114*B;%Here is the luminance.
-Cr = R - Y;
-Cb=B-Y;
-figure, montage({Y,Cr,Cb})
-%Start Bais P(A|C) =  (P(C|A)P(A))/P(C)  
-% P(C) is the normalizing constant
-%P(A) Prior Probability 
-%P(C|A) Likelihood 
-%P(A|C) How probabile it is what we think it is. 
-%TO solve Zero Frequency: Laplace Estimation or adding 1 for simple cases
-%to avoid dividing by zero
-%Might be helpful to do Gaussin distribution.
-
-%Just split the picture in half.
-%Right Side
-imhist(Y)
-[row,col] = size(Y);
-middleColumn = floor(col/2);
-rightHalfIlluminate = Y(:,middleColumn+1:end);
-[pixelCountR,grayLevelsR] = imhist(rightHalfIlluminate);
-%Left side
-leftHalfIlluminate = Y(:,1:middleColumn);
-[pixelCountL,grayLevelsL] = imhist(leftHalfIlluminate);
-figure, montage({rightHalfIlluminate,leftHalfIlluminate})
-
-%Difference should equal the threshold
-diffhist = pixelCountL - pixelCountR;
-threshold = 255*graythresh(diffhist);
-%Find pixels above the threshold
-aboveThresh = Y >threshold;
-figure,imshow(aboveThresh)%Well, this just got interesting...
 %%
-test1 = aboveThresh - darkSkin;%here is what I'm masking out 
-test2 = aboveThresh+darkSkin;
-figure,montage({test1,test2})
-
-edgeLapTest = laplacianFilter2(test1);
-figure,imshow(edgeLapTest)
-
-skin = darkSkin+edgeLapTest;
+skin = skinDetect(picDark);
 figure,imshow(skin)
+%Invert for future use
+%invertedSkin = invert(skin);
+%figure,imshow(invertedSkin)
+%%
+%Convert to YCbCr
+imYUV=colorspace('yuv<-rgb',picDark);
+
+%Yone = imYUV(:,:,1);
+%YCb = imYUV(:,:,2);
+%YCr = imYUV(:,:,3);
 
 
+
+
+%% 
+%I'm doing this part incorrectly, and I'm unsure what to do.  I know i need
+%the sum.
+h = myhist(picDark);%imhist(Y)%myhist
+
+figure,plot(h)
+%Loop to find the threshold
+var=zeros(256,1); 
+index2=0;%Get sum.
+for index = 20 : length(h)-20
+    %text = length(h)-20;%Subtract 20 because I take off 10 from back and 10 from front
+    index2 = index2 + h(index-1)+h(index-2)+h(index-4)+h(index-8) - h(index) +h(index+1)+h(index+2)+h(index+4)+h(index+8);
+end
+%%
+threshold = index2/10;%I know that there has to be a better way.  But I'm unsure how to get a good threshold using the histogram information.
+
+%%
+
+[rowX, colY] = size(imYUV);
+for row=1:rowX
+    for col=1:colY 
+        if imYUV(row,col)> threshold %textThat %I know that textThat needs to be (.6)
+            imYUV(row,col) = 0;
+        end
+    end
+end
+figure,imshow(imYUV)
+%%
+
+
+im_new=colorspace('rgb<-yuv',imYUV);
+
+finalFinal = im_new .* skin;
+figure,imshow(finalFinal)
+
+
+
+
+
+%% 
+% Given Code From blackboard
 function skin = skinDetect(pic)
     %given code in the assignment details.
     %All this does is detect skin pixels using RGB
@@ -71,17 +81,48 @@ function skin = skinDetect(pic)
     ims2 = (pic(:,:,1)-pic(:,:,2)>15) | (pic(:,:,1)-pic(:,:,3)>15);
     ims3 = (pic(:,:,1)-pic(:,:,2)>15) & (pic(:,:,1)>pic(:,:,3));
     ims = ims1 & ims2 & ims3;
-    figure,imshow(ims)
+    %figure,imshow(ims)
     skin = ims;
 end
-function lapLacian = laplacianFilter2(usePic)
-    laplFilt=[-1 -1 -1; 
-              -1 8 -1; 
-              -1 -1 -1];
-    %laplacianFilt = filter2(usePic,laplFilt,'self');
-    lapLacian = conv2(usePic,laplFilt,'same');%This was lapLacianFilt
-   
+
+function sobelKernel = sobFilter(usePic)
+    sobFiltOne=[-1 0 +1;
+                -2 0 +2;
+                -1 0 +1];%xDirection is done
+    sobFiltTwo=[+1 +2 +1;
+                 0 0 0;
+                -1 -2 -1];%yDirection is done
+    %sobelFiltOneImage = imfilter(usePic,sobFiltOne,'same');
+    %sobelFiltTwoImage = imfilter(usePic,sobFiltTwo,'same');
+        %Working is above.
+    sobelFiltOneImage = conv2(usePic,sobFiltOne);%Another function to loop through picture to apply Kernel
+    sobelFiltTwoImage = conv2(usePic,sobFiltTwo);
+    %sobelFilt#Image = conv2(usePic,sobfilt#,'full');
+        %Why does filter2 flip the image upside down?
+            %conv2(pic, mask) = filter2(rot90(mask,2), pic)
+            %conv2 is a bit faster than fitler, no reason for me to use
+            %filter2 here.  
+    
+    sobelKernel = sqrt(sobelFiltOneImage.^2 + sobelFiltTwoImage.^2);
+      %Edges detected better due to reading the image as a double from the
+      %begging. 
 end
+
+function med = medFilter(usePic)%There has to be a better way to implement this...
+    [rows, col] = size(usePic);%size of grayscale image
+    picSalt = usePic;%Unsure why I did this, to lazy to change now. 
+    pad=zeros(rows,col);
+    for i=2:rows-1
+        for j=2:col-1
+            %Make 3x3 mask
+            filter = [picSalt(i-1,j-1),picSalt(i-1,j),picSalt(i-1,j+1),picSalt(i,j-1),picSalt(i,j),picSalt(i,j+1),picSalt(i+1,j-1),picSalt(i+1,j),picSalt(i+1,j+1)];
+            pad(i,j)= median(filter);%function that just return median value
+        end
+    end
+    med = pad;
+end
+
+
 
 
     
